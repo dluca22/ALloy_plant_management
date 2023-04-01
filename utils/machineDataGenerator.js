925aef1dce8f89dd663e27248177ef30639d655d
dluca22 <luc.due@tim.it>
2023-04-01 17:04:28 +0200

    fix, only emit data for machine that are online

--- utils/machineDataGenerator.js
+++ utils/machineDataGenerator.js
@@ -14,51 +14,54 @@
 // gettin machines from database
 
 // import database
-const db = require('../database')
+const db = require('../database');
 
 // async gets operational_params from database and returns a promise
-function getMachineData(){
-return new Promise((resolve, reject) =>{
-
-    db.query('SELECT m.id, m.name, p.min_temp, p.max_temp, p.flow_rate, p.min_pressure, p.max_pressure  FROM machines m JOIN operational_parameters p ON p.machine_id = m.id', (error, result) => {
+function getMachineData() {
+  return new Promise((resolve, reject) => {
+    db.query(
+      'SELECT m.id, m.name, m.online, p.min_temp, p.max_temp, p.flow_rate, p.min_pressure, p.max_pressure  FROM machines m JOIN operational_parameters p ON p.machine_id = m.id',
+      (error, result) => {
         if (error) {
-            console.log('errore in query machinesDataGenerator')
-            reject(error)
+          console.log('errore in query machinesDataGenerator');
+          reject(error);
+        } else {
+          resolve(result);
         }
-        else {
-            resolve(result)
-        }
-    })
-})
-
+      }
+    );
+  });
 }
 
 async function machineDataGenerator(socket) {
+  let machinesData;
 
-    let machinesData;
+  if (!machinesData) {
+    machinesData = await getMachineData();
+    console.log('ricerca db');
+  }
 
-    if(!machinesData){
-        machinesData = await getMachineData()
-        console.log("ricerca db")
-    }
+  // console.log("quiiii", machinesData)
 
-    // console.log("quiiii", machinesData)
+  // for each machine received from database, on a 3 sec interval, emit a socket message named after the machine with updated data from
+  const interval = setInterval(() => {
+    machinesData.forEach((machine) => {
 
-
-    // for each machine received from database, on a 3 sec interval, emit a socket message named after the machine with updated data from
-    const interval = setInterval(() => {
-    machinesData.forEach(machine => {
-// TODO continuare qua a creare un emit per ogni macchina, ma controllare se ha senso
+      if (machine.online) {
         socket.emit(machine.name, {
-            pressure : generateRandomData(machine.min_pressure, machine.max_pressure),
-            temperature : generateRandomData(machine.min_temp, machine.max_temp),
-            flow_rate : generateRandomFlowRate(machine.flow_rate),
-        })
-      })
-      console.log("emit")
+          pressure: generateRandomData(
+            machine.min_pressure,
+            machine.max_pressure
+          ),
+          temperature: generateRandomData(machine.min_temp, machine.max_temp),
+          flow_rate: generateRandomFlowRate(machine.flow_rate),
+        });
+      }
+    });
+    console.log('emit');
   }, 3000);
   socket.on('disconnect', () => {
-    clearInterval(interval)
+    clearInterval(interval);
     console.log('a user disconnected');
   });
   console.log('chiamata');
@@ -75,15 +78,14 @@ module.exports = machineDataGenerator;
 // generate random number between an interval of max & min inclusive with 1 decimal
 // TODO aggiungere casualità di numeri al di fuori del bracket minimo-massimo, esempio 2% di casistica che siano valori maggiori o minori del minimo o massimo (tipo moltiplicatore differenziale)
 function generateRandomData(min, max) {
-
   const result = Math.random() * (max - min) + min;
-  return Number(result.toFixed(1))
+  return Number(result.toFixed(1));
 }
 
 // Generates a random number within a range of +/- 3 from the input value with 3 decimal
 // TODO change fixed number to fixed percent from base number (means nothing adding +/-3 if one machine's value is 500 and another is 1 )
-function generateRandomFlowRate(val){
-  const delta = Math.random() *(3 - (-3)) + (-3)
-  const result = val + delta
-	return Number((result).toFixed((3)))
+function generateRandomFlowRate(val) {
+  const delta = Math.random() * (3 - -3) + -3;
+  const result = val + delta;
+  return Number(result.toFixed(3));
 }
